fbc5016ad984807be099ed83c8fa990264870069
nerdbattle1 <36824033+nerdbattle1@users.noreply.github.com>
2018-02-25 20:59:09 -0800

    Basic gravity gun

---
+++ Assets/Scripts/Models/Behavior/BehaviorGravity.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BehaviorGravity : BehaviorTimed
+{
+    const float kAntiGravity = 15.0f;
+
+    private Rigidbody mRigidbody;
+
+    public BehaviorGravity(Entity entity)
+        : base(entity, 3f)
+    { }
+
+    public override string GetDebugName()
+    {
+        return "Gravity";
+    }
+
+    public override bool IsMovement()
+    {
+        return true;
+    }
+
+    public override void Start()
+    {
+        base.Start();
+
+        mRigidbody = mEntity.GetComponent<Rigidbody>();
+    }
+
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+
+        mRigidbody.AddForce(Vector3.up * kAntiGravity);
+    }
+
+    public override void Stop()
+    {
+        base.Stop();
+
+        mRigidbody.AddForce(Vector3.up * -kAntiGravity, ForceMode.Impulse);
+    }
+}
